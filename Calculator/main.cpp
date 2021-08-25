a9ddafb93f98407517d3bfbbf224f6fbd1518827
eassuncao <86193196+eassuncao@users.noreply.github.com>
2021-08-24 23:05:28 -0400

    Add files via upload

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -14,10 +14,12 @@ int main()
     cout<<"This calculator supports two numbers at a time and four operators."<<endl<<endl;
     cout<<"Please type the first number in your calculation, then hit Enter:"<<endl;
     cin>>num1;
-    cout<<"Now type + to add, - to subtract, * to multiply, or / to divide, then hit Enter:"<<endl;
+    cout<<"Now enter the operator:"<<endl<<"+ for addition"<<endl<<"- for subtraction"<<endl<<"* for multiplication"<<endl<<"/ for division"<<endl<<
+    "^ for power"<<endl<<"v for root"<<endl<<"then hit Enter:"<<endl;
     cin>>sign;
-    cout<<"Please type the second number in your calculation, then hit Enter:"<<endl;
+    cout<<"Please type the number that will be used by the operator, then hit Enter:"<<endl;
     cin>>num2;
+    cout<<endl;
 
     if(sign == "+"){
         cout<<"Your result is: "<<num1+num2;
@@ -33,6 +35,32 @@ int main()
 
     else if(sign == "/"){
         cout<<"Your result is: "<<num1/num2;
+    }
+
+    else if(sign == "^"){
+
+        double resultPower = 1;
+
+        for(double exponent = num2; exponent != 0; exponent --){
+
+            resultPower *= num1;
+
+        }
+
+        cout<< "Your result is: " << resultPower << endl;
+    }
+
+    else if(sign == "v"){
+
+        double resultRoot = 1;
+
+        for(double exponent = num2; exponent != 0; exponent --){
+
+            resultRoot /= num1;
+
+        }
+
+        cout<< "Your result is: " << resultRoot << endl;
     }
 
     else {
